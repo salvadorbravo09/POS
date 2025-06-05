2fcbb8708506351b054dfa8fcc47bedc4aab761f
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-04 23:06:08 -0400

    refactor: creando un resource para categorias

--- api/src/categories/categories.module.ts
+++ api/src/categories/categories.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
