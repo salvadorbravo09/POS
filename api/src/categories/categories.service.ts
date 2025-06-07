6ceb493ffd2210ac024dc48dd86e52e747d00b12
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-07 17:54:55 -0400

    feat: registrando el repositorio en el service

--- api/src/categories/categories.service.ts
+++ api/src/categories/categories.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
-    return createCategoryDto;
+    console.log(createCategoryDto);
+    return 'This action adds a new category';
   }
 
   findAll() {
