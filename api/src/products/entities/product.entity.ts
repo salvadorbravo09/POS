c3d750fe28fc9e0c9ef27ceb2e48c60b50452381
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-08 15:34:40 -0400

    feat: relacionar entidades de categoria y producto con typeorm

--- api/src/products/entities/product.entity.ts
+++ api/src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -21,4 +22,7 @@ export class Product {
 
   @Column({ type: 'int' })
   inventory: number;
+
+  @ManyToOne(() => Category)
+  category: Category;
 }
