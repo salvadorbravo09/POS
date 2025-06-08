672cd147e1a564bd4fd000a67c5f35218df6e33a
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-08 14:30:50 -0400

    feat: creando el entity de productos

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { typeOrmConfig } from './config/typeorm.config';
       inject: [ConfigService],
     }),
     CategoriesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ api/src/products/entities/product.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 120,
+    nullable: true,
+    default: 'default.svg',
+  })
+  image: string;
+
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @Column({ type: 'int' })
+  inventory: number;
+}
