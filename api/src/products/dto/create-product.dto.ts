5716c41d32683bbe23e3368a7b7a2524bc0e287b
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-08 16:39:57 -0400

    feat: añadiendo validacion a productos

--- api/src/products/dto/create-product.dto.ts
+++ api/src/products/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'El nombre del producto es obligatorio' })
+  @IsString({ message: 'El nombre del producto debe ser texto' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El precio del producto es obligatorio' })
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'El precio debe ser un número con máximo 2 decimales' },
+  )
+  price: number;
+
+  @IsNotEmpty({ message: 'La cantidad del producto es obligatoria' })
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: 'La cantidad debe ser un número entero' },
+  )
+  inventory: number;
+
+  @IsNotEmpty({ message: 'La categoría del producto es obligatoria' })
+  @IsInt({ message: 'La categoría debe ser un número entero válido' })
+  categoryId: number;
+}
