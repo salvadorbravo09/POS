322730d1d376c2c4a6d38d1492f1071c37d4a11d
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-17 21:34:24 -0400

    feat: añadiendo validacion a transaction

--- api/src/transactions/dto/create-transaction.dto.ts
+++ api/src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,35 @@
-export class CreateTransactionDto {}
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  ValidateNested,
+} from 'class-validator';
+
+export class TransactionContentsDto {
+  @IsNotEmpty({ message: 'El ID del producto es obligatorio' })
+  @IsInt({ message: 'Producto no valido' })
+  productId: number;
+
+  @IsNotEmpty({ message: 'La cantidad es obligatoria' })
+  @IsInt({ message: 'Cantidad no valida' })
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Precio no puede estar vacio' })
+  @IsNumber({}, { message: 'Precio no valido' })
+  price: number;
+}
+
+export class CreateTransactionDto {
+  @IsNotEmpty({ message: 'El total no puede ir vacio' })
+  @IsNumber({}, { message: 'Cantidad no valida' })
+  total: number;
+
+  @IsArray()
+  @ArrayNotEmpty({ message: 'Los contenidos no pueden ir vacios' })
+  @ValidateNested()
+  @Type(() => TransactionContentsDto)
+  contents: TransactionContentsDto[];
+}
